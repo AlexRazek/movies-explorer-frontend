163f7ac5b85973d28321af4676ea801c9e1e388a
Alexander Volotchin <alexrazek@yandex.ru>
2023-09-07 16:29:42 +0300

    Diplom edit after final rev

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -10,7 +10,7 @@ function MoviesCard(props) {
   const location = useLocation();
   const userMoviesUrl = `${moviesURL}${props.image.url}`;
 
-  const savedUserMovies = (JSON.parse(localStorage.getItem("userMovies")) || []).find((item) => item.nameRU === props.card.nameRU);
+  const savedUserMovies = JSON.parse(localStorage.getItem("userMovies")).find((item) => item.nameRU === props.card.nameRU);
 
   const isLiked = Boolean(savedUserMovies); 
 
